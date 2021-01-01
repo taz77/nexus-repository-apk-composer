0eb04d0b6213b311a0259f33b11845a7c63bbaf0
Brady Owens <info@fastglass.net>
2021-01-01 12:27:30 -0500

    Remove old Travis variables

--- release.sh
+++ release.sh
@@ -2,16 +2,9 @@
 
 set -e
 
-if [[ "${TRAVIS_PULL_REQUEST}" == "false" && ("${TRAVIS_BRANCH}" == "master"  || -n "${TRAVIS_TAG}") ]]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
-    if [[ -n "${TRAVIS_TAG}" ]]; then
-        export STABILITY_TAG="${TRAVIS_TAG}"
-    fi
+IFS=',' read -ra tags <<< "${TAGS}"
 
-    IFS=',' read -ra tags <<< "${TAGS}"
-
-    for tag in "${tags[@]}"; do
-        make release TAG="${tag}";
-    done
-fi
+for tag in "${tags[@]}"; do
+    make release TAG="${tag}";
